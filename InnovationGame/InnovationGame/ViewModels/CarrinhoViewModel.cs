7418569674de735b16b0d484ad3ad97db4737ab3
Ester Ribeiro <ester.ribeiro@programmers.com.br>
2020-08-12 14:13:22 -0300

    Primeiro commit

---
+++ InnovationGame/InnovationGame/ViewModels/CarrinhoViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace InnovationGame.ViewModels
+{
+    public class CarrinhoViewModel
+    {
+        public int Id { get; set; }
+        public ProdutoViewModel Produto { get; set; }
+        public int Quantidade { get; set; }
+        public string Endereco { get; set; }
+        public string NumeroCartao { get; set; }
+        public string DataValidade { get; set; }
+        public string CVV { get; set; }
+    }
+}
---
+++ InnovationGame/InnovationGame/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InnovationGame.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Foto { get; set; }
+        public double Preco { get; set; }
+    }
+}
